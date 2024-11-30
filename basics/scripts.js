e369a488da21f282fc8eb83ca498ae37f67e2613
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2024-11-30 19:41:45 +0100

    working play, getTierLevel, printTierLevel functions and peldanyositas

--- basics/scripts.js
+++ basics/scripts.js
@@ -5,12 +5,34 @@ function Player(nickname){
 
 Player.prototype.play = function(){
     this.playedMatch++;
-    console.log(this.nickname `played` + this.playedMatch `matches`);
+    console.log(this.nickname  + ` played ` + this.playedMatch  +  ` matches.`);
 };
 
-const player = new Player(`Haaland`);
+
 
 Player.prototype.getTierLevel = function(){
 
-    if(this.playedMatch)
+    if(this.playedMatch <= 3){
+        return `A`;
+    }
+    else if(this.playedMatch <= 6){
+        return `B`;
+    }
+    else{
+        return `C`;
+    }
 };
+
+/**
+ * 
+ * @param {string} playerName 
+ * @param {string} tierLevel 
+ */
+
+function printTierLevel(playerName, tierLevel){
+ console.log(playerName + ` is in the ` + tierLevel + ` tier.`);
+};
+
+const player = new Player(`Dzsudzsi`);
+player.play();
+printTierLevel(`Dzsudzsák Balázs`, `99`);
