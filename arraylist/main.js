337aa3c0e90da8b6bde74ed214c7a43e5168a6dd
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-01-21 19:24:54 +0100

    11es feladat környékéig jutottam

--- arraylist/main.js
+++ arraylist/main.js
@@ -14,9 +14,9 @@ class ArrayList{
     Add(element){
         const lengths = this.#length_of_elements;
 
-        this.#status[index] = element;
+        this.#status[lengths] = element;
 
-        Object.defineProperty(this, index, {
+        Object.defineProperty(this, lengths, {
             get: function(){
                 return this.#status[index];
             }, 
@@ -28,4 +28,23 @@ class ArrayList{
 
         this.#length_of_elements++;
     }
+
+    Clear(){
+        this.#status = {};
+        this.#length_of_elements = 0;
+       /* for(const kulcs in this){
+            delete this[kulcs];
+        }*/
+    }
 }
+
+const tomblista = new ArrayList();
+
+const elem1 = {name: `Paradicsom`};
+const elem2 = {name: `Uborka`};
+
+tomblista.Add(elem1);
+tomblista.Add(elem2);
+
+tomblista.Clear();
+
