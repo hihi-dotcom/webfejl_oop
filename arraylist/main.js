0def65e79f2670f61ba23092d4c2e8fc147093ae
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-01-21 18:48:26 +0100

    Add function

--- arraylist/main.js
+++ arraylist/main.js
@@ -1,7 +1,7 @@
 class ArrayList{
-    #status;
     #length_of_elements;
-
+    #status;
+    
     constructor(){
         this.#status = {};
         this.#length_of_elements = 0;
@@ -9,5 +9,23 @@ class ArrayList{
 
     get Count(){
         return this.#length_of_elements;
+    }
+
+    Add(element){
+        const lengths = this.#length_of_elements;
+
+        this.#status[index] = element;
+
+        Object.defineProperty(this, index, {
+            get: function(){
+                return this.#status[index];
+            }, 
+            set: function(ertek){
+                this.#status[index] = ertek;
+            },
+            enumerable: true,
+        })
+
+        this.#length_of_elements++;
     }
 }
