8dc33a9d1f3bee86967019aa9a76459ce4d33263
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-01-21 19:51:43 +0100

    restarting

--- arraylist/main.js
+++ arraylist/main.js
@@ -16,38 +16,12 @@ class ArrayList{
 
         this.#status[lengths] = element;
 
-        Object.defineProperty(this, lengths, {
-            get: function(){
-                return this.#status[lengths];
-            }, 
-            set: function(ertek){
-                this.#status[lengths] = ertek;
-            },
-            enumerable: true,
-        })
 
-        this.#length_of_elements++;
-    }
 
     Clear(){
         this.#status = {};
         this.#length_of_elements = 0;
-        /*for(const kulcs in this){
-            delete this[kulcs];
-        }*/
+
     }
 }
-
-const tomblista = new ArrayList();
-
-const elem1 = {name: `Paradicsom`};
-
-
-tomblista.Add(elem1);
-
-const elem2 = {name: `Uborka`};
-tomblista.Add(elem2);
-
-tomblista.Clear();
-
-console.log(tomblista);
+};
