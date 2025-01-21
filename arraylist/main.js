ffd81dde8cbad9a3b2b211ea3ed11d8cd630c81d
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-01-21 21:43:21 +0100

    all done

--- arraylist/main.js
+++ arraylist/main.js
@@ -14,12 +14,35 @@ class ArrayList{
     Add(element){
         const lengths = this.#length_of_elements;
         this.#status[lengths] = element; 
-
+        Object.defineProperty(this, lengths,  {
+            get: function(){
+                return this.#status[lengths];
+            },
+            set: function(uj_ertek){
+                this.#status[lengths] = uj_ertek;
+            },
+            
+            enumerable: true,
+            configurable: true
+        });
         this.#length_of_elements++;
     };
     Clear(){
         this.#status = {};
         this.#length_of_elements = 0;
+
+        for(const kulcsok in this){
+            delete this[kulcsok];
+        }
+    };
+    
+    Contains(vizsg){
+        for(let i = 0; i < this.Count; i++){
+            if(this.#status[i] == vizsg){
+                return true;
+            }
+        }
+        return false;
     };
 };
 
@@ -27,7 +50,19 @@ const tomblista = new ArrayList();
 
 const elsoobj = {name: "Paradicsom"};
 const masodikobj = {sorszam: 2};
+const harmadikobj = {name: "Uborka"};
+const negyedikobj = {sorszam: 88};
+
+elsoobj.name = "Lajos";
+
+masodikobj.sorszam = 99;
 tomblista.Add(elsoobj);
 tomblista.Add(masodikobj);
+tomblista.Add(harmadikobj);
+tomblista.Add(negyedikobj);
+
+console.log(tomblista.Contains(elsoobj));
+console.log(tomblista.Contains(masodikobj));
+
+tomblista.Clear();
 console.log(tomblista);
-console.log(tomblista[2]);
