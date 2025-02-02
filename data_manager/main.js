cc02d0a692012e26742460154f5a31059758fe81
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-02-02 21:17:51 +0100

    commenting is done

--- data_manager/main.js
+++ data_manager/main.js
@@ -43,7 +43,7 @@ class Data_Manager{
     add(Person){//létrehoztam egy add függvényt, amelynek egy paramétere van(Person), és a típusa is egy Person(korábban definiáltuk)
         this.#array.push(Person);//hozzáadjuk a tömb privát paraméterünkhöz, a push-al a bemeneti paraméterünket, ami a Person(Person típusú) 
 
-        this.#updateCallback(this.#array);
+        this.#updateCallback(this.#array);//meghívtuk a privát tulajdonságunkat,(függvényként) amely tartalmazza a definiált callback függvényünket, és megadtuk neki a privát tömbünket
     }
 
 /**
@@ -58,7 +58,7 @@ class Data_Manager{
                 result_ages.push(this.#array[i]); //ha megegyezik, a push segítségével belerakjuk a tömbünk atuális elemét a függvény első sorában létrehozott tömbbe
             }
         }
-        this.#updateCallback(result_ages);
+        this.#updateCallback(result_ages);//meghívtuk a privát tulajdonságunkat,(függvényként) amely tartalmazza a definiált callback függvényünket, és megadtuk neki a tömbünket, amely a jó objektumokat tartalmazza
     }
 
     /**
@@ -74,31 +74,31 @@ class Data_Manager{
            
                 }
         
-            this.#updateCallback(result_names);
+            this.#updateCallback(result_names);//meghívtuk a privát tulajdonságunkat,(függvényként) amely tartalmazza a definiált callback függvényünket, és megadtuk neki a tömbünket, amely a jó objektumokat tartalmazza
         }
 
 
 }
 
-class Data_Table{
+class Data_Table{//definiáltam a Data_Table osztályt
     /**
      * @type {HTMLTableSectionElement}
      */
-    #tbody;
+    #tbody;//deklaráltam egy tbody nevű privát változót, amelynek megadtam a típusát(HTMLTableSectionElement)
     /**
      * 
      * @param {Data_Manager} datamanager 
      */
-    constructor(datamanager){
-        const table = document.createElement(`table`);
-        document.body.appendChild(table);
+    constructor(datamanager){//létrehoztam a Data_Table osztály konstruktorát, amelynek egy paraméterje van a datamanager amelynek típusa megegyezik az előző osztályunkéval
+        const table = document.createElement(`table`); //Létrehoztam egy table HTMLElementet
+        document.body.appendChild(table);//Hozzáfűztem a HTML oldalamhoz a table HTMLElementet
 
-        const tbody = document.createElement(`tbody`);
-        table.appendChild(tbody);
-        this.#tbody = tbody;
+        const tbody = document.createElement(`tbody`); //Létrehozok egy tbody HTMLElementet
+        table.appendChild(tbody);//Hozzáfűzöm a most létrehozott tbody elemet a table HTMLElementhez
+        this.#tbody = tbody;// a létrehozott tbody privát tulajdonságomnak értékül adom, a  most létrehozott tbody HTMLElementet
 
-        datamanager.setUpdateCallback((emberek) => {
-            this.#renderTable(emberek);
+        datamanager.setUpdateCallback((emberek) => {//meghívtuk a bemeneti paraméterünk setUpdateCallback függvényét, amelynek paraméterként megadtunk egy üres arrow függvényt, amelynek megadtuk paraméterként az emberek tömböt, amely Person típusú objektumokat tárol 
+            this.#renderTable(emberek); //meghívtuk a renderTable privát függvényt, amelynek megadtuk paraméterként az emberek tömböt, amely az arrow function paraméterei közül érkezett(Person)
         });
     }
 
@@ -107,19 +107,19 @@ class Data_Table{
      * @param {Person} emberes_tomb 
      */
 
-    #renderTable(emberes_tomb){
-        this.#tbody.innerHTML = ``;
-        for(const ember of emberes_tomb){
-            const row = document.createElement(`tr`);
-            this.#tbody.appendChild(row);
+    #renderTable(emberes_tomb){//deklaráltam a renderTable privát függvényt, amelynek egy paramétere van amely a Person típusú objektumokat tároló emberes_tomb
+        this.#tbody.innerHTML = ``; //a deklarált privát tulajdonság(HTMLELement) innerHTML függvényének megadom értékül az üres stringet
+        for(const ember of emberes_tomb){//végigiterálok a paraméterként kapott tömbön egy for of ciklussal
+            const row = document.createElement(`tr`); //létrehozok egy tr elemet(HTMLELement)
+            this.#tbody.appendChild(row);//hozzáfűzöm a tbody privát tulajdonsághoz, a most létrehozott tr elemet
 
-            const cella1 = document.createElement(`td`);
-            row.appendChild(cella1);
-            cella1.innerHTML = ember.nev;
+            const cella1 = document.createElement(`td`);//Létrehozok egy td elemet(HTMLElement)
+            row.appendChild(cella1);//Hozzáfűzöm a tr elemhez a most létrehozott td cellát
+            cella1.innerHTML = ember.nev;//a most létrehozott td elem innerHTML tulajdonságának értékül adom az aktuális elem nev tulajdonságát
 
-            const cella2 = document.createElement(`td`);
-            row.appendChild(cella2);
-            cella2.innerHTML = ember.eletkor;
+            const cella2 = document.createElement(`td`);//létrehozok megint egy td elemet(HTMLElement)
+            row.appendChild(cella2);//Hozzáfűzöm a második létrehozott td elemet a sorhoz, ami a tr-t jelenti
+            cella2.innerHTML = ember.eletkor;// a cella (td) innerHTML tulajdonságának értékül adom az aktuális elem eletkor tulajdonságát
 
         }
     }
@@ -127,39 +127,36 @@ class Data_Table{
 
 const adat_manager = new Data_Manager([{nev: `Feri`, eletkor: 17},
     {nev: `Géza`, eletkor: 17},
-    {nev: `Józsi`, eletkor: 16}]);
+    {nev: `Józsi`, eletkor: 16}]);//Példányosítottam a Data_Manager osztályt, amelynek megadtam paraméterként egy névnélküli tömböt, amely Person objektumokat tartalmaz,hármat.
 
 
-const adat_table = new Data_Table(adat_manager);
+const adat_table = new Data_Table(adat_manager); //Példányosítottam a Data_Table osztályunkat, amelynek paraméterül megadom a korábban példányosított Data_Manager osztályt tartalmazó változó nevét
 
 //+
-const nev_label = document.createElement(`label`);
-nev_label.innerText = `Név: `;
-document.body.appendChild(nev_label);
+const nev_label = document.createElement(`label`);//Létrehozok egy labelt(HTMLElement)
+nev_label.innerText = `Név: `;// A label elem Innertext tulajdonságának értékül stringet adok 
+document.body.appendChild(nev_label);//hozzáfűzöm a HTML fájlhoz, a létrehozott labelt
 
 
 //11. pont
-const input_1 = document.createElement(`input`);
-document.body.appendChild(input_1);
-const breaking_html = document.createElement(`br`);
-document.body.appendChild(breaking_html);
-
-const kor_label = document.createElement(`label`);
-kor_label.innerText = `Kor: `;
-document.body.appendChild(kor_label);
-const input_2 = document.createElement(`input`);
-document.body.appendChild(input_2);
+const input_1 = document.createElement(`input`);//létrehozok egy input elemet(HTMLElement)
+document.body.appendChild(input_1);//hozzáfűzöm a HTML dokumentumhoz az input elemet
+const kor_label = document.createElement(`label`);//létrehozok egy label elemet megint(HTMLElement)
+kor_label.innerText = `Kor: `; //a label innerText tulajdonságának megadom a stringet
+document.body.appendChild(kor_label);//a most létrehozott label elemet, hozzáfűzöm a HTML dokumentumhoz
+const input_2 = document.createElement(`input`);//létrehozok még egy input elemet(HTMLElement)
+document.body.appendChild(input_2);// a második input elemet hozzáfűzzük a HTML dokumentumhoz
 
 
 
-input_1.addEventListener(`input`, (e) =>{
+input_1.addEventListener(`input`, (e) =>{//létrehozok az elsőként létrehozott input elemnek, egy addEventListenert, amely az input eseményre hallgat, ha bekövetkezik
  
-    adat_manager.filterName(input_1.value);
+    adat_manager.filterName(input_1.value); //meghívjuk a Data_Manager osztályunk példányának a filterName függvényét, amelynek paraméterként átadom az első input értékét
 
 });
 
-input_2.addEventListener(`input`, (e) =>{
+input_2.addEventListener(`input`, (e) =>{//létrehozok a másodiként létrehozott input elemnek, egy addEventListenert, amely az input eseményre hallgat, ha bekövetkezik
     
-    const keresett_szam = Number(input_2.value);
-    adat_manager.filterAge(keresett_szam);
+    const keresett_szam = Number(input_2.value);//egy változóban eltároljuk, a Number() függvény segítségével átkonvertált második input értékét
+    adat_manager.filterAge(keresett_szam);//meghívjuk a Data_Manager osztályunk példányának a filterName függvényét, amelynek paraméterként átadom a változót, amelyben a második input átkonvertált értékét tároltam
 });
