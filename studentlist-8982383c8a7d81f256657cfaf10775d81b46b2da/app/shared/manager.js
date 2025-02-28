02a5566e1caaad098aafffcf917ba911328265e9
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-02-28 10:08:30 +0100

    StudentList is working

--- studentlist-8982383c8a7d81f256657cfaf10775d81b46b2da/app/shared/manager.js
+++ studentlist-8982383c8a7d81f256657cfaf10775d81b46b2da/app/shared/manager.js
@@ -17,10 +17,19 @@ class Manager{
 
     add(student){
         this.#array.push(student);
-        this.#addCallback(student);
+        
     }
 
     select(student){
         this.#selectCallback(student);
+    }
+
+    /**
+     * Végigiterál a tömbön és meghívja az addCallbacket
+     */
+    render(){
+        for(const student of this.#array){
+            this.#addCallback(student);
+        }
     }
 }
